df41fd02c5ce918ef548a1a7560378a7ff96d372
zhangsan <1299926735@qq.com>
2020-03-01 16:08:06 +0800

    更改的代码

--- day24_git02/src/com/itheima/git/demo1.java
+++ day24_git02/src/com/itheima/git/demo1.java
@@ -4,5 +4,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("demo1");
         System.out.println("master");
+        System.out.println("添加的代码");
     }
 }
