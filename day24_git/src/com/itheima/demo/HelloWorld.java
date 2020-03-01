aa0fc38dbac5a25b82d02256ca1946ada81014ab
zhangsan <1299926735@qq.com>
2020-03-01 16:08:37 +0800

    添加的内容

--- day24_git/src/com/itheima/demo/HelloWorld.java
+++ day24_git/src/com/itheima/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itheima.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,java");
+        System.out.println("添加的代码");
     }
 }
